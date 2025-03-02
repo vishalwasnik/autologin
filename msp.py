9a5178c178a36160f0b73435c5bdcb9f31693545
vishalwasnik <vishalwasnik121@gmail.com>
2025-03-02 20:36:42 +0530

    msp.py

--- msp.py
+++ msp.py
@@ -1,1 +1,21 @@
 
+import pyautogui
+import time
+
+pyautogui.hotkey('win','r')
+pyautogui.write('msedge')
+pyautogui.hotkey('enter')
+time.sleep(1)
+pyautogui.write('https://www.instagram.com/accounts/login/')
+pyautogui.hotkey('enter')
+time.sleep(5)
+pyautogui.hotkey('tab')
+pyautogui.write('instagram')
+pyautogui.hotkey('tab')
+pyautogui.write('------------')
+pyautogui.hotkey('tab')
+pyautogui.hotkey('tab')
+pyautogui.hotkey('enter')
+
+time.sleep(5)
+pyautogui.hotkey('alt','f4')
